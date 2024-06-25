e15b42e8fed51858858f93bf11d6ec394fd3f65e
Risang Baskoro <contact@risangbaskoro.com>
2024-06-25 23:31:08 +0700

    Update classifiers, move version to __init__.py

--- icvlp/__init__.py
+++ icvlp/__init__.py
@@ -1,6 +1,6 @@
-from .__version__ import __version__
-
 from .downloader import VideoDownloader
 
 from .object import ICVLP
 from .object import *
+
+__version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from icvlp.__version__ import __version__ as ver
 from setuptools import setup
+
+import icvlp
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-# TODO: Add license.
-
 setup(
     name='icvlp-dataset',
-    version=ver,
+    version=icvlp.__version__,
     packages=['icvlp'],
     url='https://github.com/risangbaskoro/icvlp-dataset',
     author='risangbaskoro',
     author_email='contact@risangbaskoro.com',
     description='Indonesian Commercial Vehicle License Plate dataset',
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    requires_python=">=3.9",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+    ]
 )
